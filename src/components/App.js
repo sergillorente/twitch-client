cd1fe8031e61804f0caae110be76412b6cb7cf27
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-02-04 19:11:04 +0100

    Routes created to navigate through the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-
-const PageOne = () => {
-    return (
-        <div>
-            Page One
-            <Link to="/pagetwo">Navigate to Page Two</Link>
-        </div>
-    );
-};
-
-const PageTwo = () => {
-    return (
-        <div>
-            Page Two
-            <button>
-                Click me!
-            </button>
-            <Link to="/">Navigate to Page One</Link>
-        </div>
-    );
-};
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
     return (
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path="/" exact component={PageOne} />
-                    <Route path="/pagetwo" component={PageTwo} />
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" component={StreamCreate} />
+                    <Route path="/streams/edit" component={StreamEdit} />
+                    <Route path="/streams/delete" component={StreamDelete} />
+                    <Route path="/streams/show" component={StreamShow} />
                 </div>
             </BrowserRouter>
         </div>
