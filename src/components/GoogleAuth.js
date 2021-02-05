a971db4c78a38584fc22288a480754add3025058
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-02-05 12:24:04 +0100

    Adding a state to the Class component in order to show if the user is logged in or not with google. Also using semantic ui terminology to style these two buttons

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,16 +1,49 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+    state = {
+        isSignedIn: null
+    };
+
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: '287089401739-c43o2nvo5dneaoj2ushactr0bhcubpei.apps.googleusercontent.com',
                 scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                this.auth.isSignedIn.listen(this.onAuthChange);
             });
         });
     }
+
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    };
+
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return null;
+        } else if (this.state.isSignedIn) {
+            return (
+                <button className="ui red google button">
+                    <i className="google icon" />
+                    Sign Out
+                </button>
+            )
+        } else {
+            return (
+                <button className="ui orange google button">
+                    <i className="google icon" />
+                    Sign in with Google
+                </button>
+            )
+        }
+    }
+
     render() {
-        return <div>Google Auth</div>
+        return <div>{this.renderAuthButton()}</div>
     };
 };
 
