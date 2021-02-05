de970c8f853c358689b89e17e9b20040ded6d8f2
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-02-05 17:52:46 +0100

    Fixing bugs in GoogleAuth regarding the auth reducer and installing and preparing for get to know redux-form

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -3,9 +3,6 @@ import { connect } from 'react-redux';
 import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends React.Component {
-    state = {
-        isSignedIn: null
-    };
 
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
@@ -14,7 +11,7 @@ class GoogleAuth extends React.Component {
                 scope: 'email'
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
-                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                this.onAuthChange(this.auth.isSignedIn.get());
                 this.auth.isSignedIn.listen(this.onAuthChange);
             });
         });
@@ -22,7 +19,7 @@ class GoogleAuth extends React.Component {
 
     onAuthChange = (isSignedIn) => {
         if (isSignedIn) {
-            this.props.signIn();
+            this.props.signIn(this.auth.currentUser.get().getId());
         } else {
             this.props.signOut();
         }
@@ -37,9 +34,9 @@ class GoogleAuth extends React.Component {
     };
 
     renderAuthButton() {
-        if (this.state.isSignedIn === null) {
+        if (this.props.isSignedIn === null) {
             return null;
-        } else if (this.state.isSignedIn) {
+        } else if (this.props.isSignedIn) {
             return (
                 <button onClick={this.onSignOutClick} className="ui red google button">
                     <i className="google icon" />
@@ -61,6 +58,11 @@ class GoogleAuth extends React.Component {
     };
 };
 
-export default connect(null, 
+const mapStateToProps = state => {
+    return { isSignedIn: state.auth.isSignedIn }
+};
+
+export default connect(
+    mapStateToProps, 
     { signIn, signOut }
     )(GoogleAuth);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer} from 'redux-form';
+import authReducer from './authReducer';
 
 export default combineReducers({
-    changeMe: () => 'here'
+    auth: authReducer,
+    form: formReducer
 });
