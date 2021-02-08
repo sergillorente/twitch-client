4f507b51955e2ecdcaae7c373abb42cfdfcb48b3
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-02-08 12:39:42 +0100

    Adding inputs to the form, taking a look and how the props are passed through the form and how to get to them to pass them as props when need it

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
+    renderInput({ input, label }) {
+        return (
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} />
+            </div>
+        )
+    }
     render() {
         return (
-            <form>
-                <Field name="title" />
-                <Field name="description" />
+            <form className="ui form">
+                <Field name="title" component={this.renderInput} label="Enter title" />
+                <Field name="description" component={this.renderInput} label="Enter description" />
             </form>
         );
     };
