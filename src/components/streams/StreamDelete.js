6528677bc788b4d11d16e28b6f3e638364f96ad6
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-02-17 12:54:54 +0100

    Refactoring the App component to update the route to follow by adding the id in the URL. Creating a Link instead of a button when pressing Delete in the List Component and finally refactoring the StreamDelete component into a class component to add it a componentDidMount and a generic message which will be different depending on what the user clicks

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,24 +1,48 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Modal from '../Modal';
+import history from '../../history';
+import { fetchStream } from '../../actions';
 
-const StreamDelete = () => {
-    const actions = (
-        <div>
-            <button className="ui button negative">Delete</button>
-            <button className="ui button">Cancel</button>
-        </div>
-    );
-    
-    return (
-        <div>
-            StreamDelete
-            <Modal 
-                title="Delete Stream"
-                content="Are you sure you want to delete this stream?"
-                actions={actions}
-            />
-        </div>
-    );
-};
+class StreamDelete extends React.Component {
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
 
-export default StreamDelete;
+    renderActions() {
+        return (
+            <>
+                <button className="ui button negative">Delete</button>
+                <button className="ui button">Cancel</button>
+            </>
+        );
+    }
+
+    renderContent() {
+        if (!this.props.stream) {
+            return 'Are you sure you want to delete this stream?'
+        }
+
+        return `Are you sure you want to delete this stream with title : ${this.props.stream.title}`
+    }
+
+    render() {
+        return (
+                <Modal 
+                    title="Delete Stream"
+                    content={this.renderContent()}
+                    actions={this.renderActions()}
+                    onDismiss={() => history.push('/')}
+                />
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] }
+}
+
+export default connect(
+    mapStateToProps,
+    { fetchStream }
+    )(StreamDelete);
